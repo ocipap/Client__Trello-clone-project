8c3c3a177f89e3e87dba4c77103d3a8de6c10d81
PAPICO <ocipap0531@gmail.com>
2019-02-11 15:31:36 +0900

    refacter: 메인 푸터 component 분리

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -178,36 +178,19 @@
           </div>
         </div>
       </section>
-      <footer class="quiet">
-        <div id="languege-picker"></div>
-        <ul>
-          <li><a href="#">Tour</a></li>
-          <li><a href="#">Pricing</a></li>
-          <li><a href="#">Apps</a></li>
-          <li><a href="#">Jobs</a></li>
-          <li><a href="#">Blog</a></li>
-          <li><a href="#">Developers</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Help</a></li>
-          <li><a href="#">Legal</a></li>
-          <li><a href="#">Cookie</a></li>
-          <li><a href="#">Settings</a></li>
-          <li><a href="#">Privacy</a></li>
-        </ul>
-        <p>
-          <img class="atlassian-logo-small" src="@/assets/images/atlassian-logo-gray-small.svg" alt="" width="150"> &nbsp; © Copyright 2019. All rights reserved.
-        </p>
-      </footer>
+      <main-footer></main-footer>
     </div>
   </div>
 </template>
 
 <script>
   import Slick from 'vue-slick'
-  
+  import MainFooter from '@/components/Footer.vue'
+
   export default {
     components: {
-      Slick
+      Slick,
+      MainFooter
     },
     data() {
       return {
@@ -546,39 +529,6 @@
     background-position: center;
     background-repeat: no-repeat;
     background-size: 1332px 250px;
-  }
-  
-  footer.quiet {
-    margin: 4em 1em;
-  }
-  
-  footer.quiet>ul {
-    text-align: center;
-  }
-  
-  footer.quiet>ul li {
-    display: inline-block;
-    margin: 0 5px;
-  }
-  
-  footer.quiet>ul li a {
-    text-decoration: underline;
-    color: #8993A4;
-    font-size: 16px;
-  }
-  
-  footer.quiet>p {
-    text-align: center;
-    margin: 8px 0;
-    font-size: 16px;
-  }
-  
-  footer.quiet>p>* {
-    display: inline-block;
-  }
-  
-  footer.quiet>p>img {
-    vertical-align: sub;
   }
   
   @media screen and (min-width: 650px) {
